5e1d5bf785e417ca7141e5c82cd1e9475142b009
HGARgG-0710 <kuznetzovigor2007@gmail.com>
2024-03-07 05:20:57 +0300

    oops

--- src/main.mjs
+++ src/main.mjs
@@ -4,27 +4,36 @@
 import { reExp, deExp, addExp } from "./api.mjs"
 import { readFile, writeFile } from "fs/promises"
 
-const regexpFromArg = (x) => RegExp(x.slice(...("/" === x[0] ? [1, x.length - 1] : [])))
+const regexpFromArg = (x) => {
+	const dashSplit = x.split("/")
+	return RegExp(
+		x.slice(
+			...("/" === x[0]
+				? [1, x.length - 1 - dashSplit[dashSplit.length - 1].length]
+				: [])
+		),
+		dashSplit[dashSplit.length - 1].split("").concat(["g"])
+	)
+}
 
 const commands = [deExp, reExp, addExp]
 
-const mode = ["delete", "de"].includes(process.argv[3])
+const mode = ["delete", "de"].includes(process.argv[4])
 	? 0
-	: ["replace", "re"].includes(process.argv[3])
+	: ["replace", "re"].includes(process.argv[4])
 	? 1
-	: ["add", "ad"].includes(mode)
+	: ["add", "ad"].includes(process.argv[4])
 	? 2
 	: (() => {
-			throw new Error(`Unknown command ${process.argv[3]} passed`)
+			throw new Error(`Unknown command ${process.argv[4]} passed`)
 	  })()
+
 await writeFile(
-	process.argv[2],
+	process.argv[3],
 	commands[mode](
-		await readFile(process.argv[1]),
+		(await readFile(process.argv[2])).toString(),
 		...process.argv
-			.slice(4)
-			.toString()
-			.split("\n")
+			.slice(5)
 			.map(
 				!mode
 					? regexpFromArg
